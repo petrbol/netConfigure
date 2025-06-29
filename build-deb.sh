c31bb8e558546884f544d4c59721a1ab485a2886
Petr Boltik <petr.boltik@gmail.com>
2025-06-29 15:41:56 +0200

    add display_name json value (generated from icinga)

--- build-deb.sh
+++ build-deb.sh
@@ -6,7 +6,7 @@
 set -e
 
 # Default version if not provided
-VERSION=${1:-"0.0.2"}
+VERSION=${1:-"0.0.3"}
 PACKAGE_NAME="netconfigure"
 MAINTAINER="Petr Boltik <petr.boltik@gmail.com>"
 DESCRIPTION="Web frontend for SSH/SCP configuration tool"
